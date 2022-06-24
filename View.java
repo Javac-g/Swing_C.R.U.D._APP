d0ab8043d34fddc84dd0a6dc7bb039df55155e73
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-24 23:23:34 +0300

    first

--- View.java
+++ View.java
@@ -5,9 +5,12 @@ import java.awt.event.ActionListener;
 
 public class View extends JFrame implements ActionListener {
     JFrame frame = new JFrame();
-    JPanel panel = new JPanel();
+    JPanel body = new JPanel();
+    JPanel header = new JPanel();
+    JPanel main = new JPanel();
+    JPanel footer =new JPanel();
 
-    //menu section
+    //body    section
     JLabel m1 = new JLabel("Menu");
     JLabel m2 = new JLabel("1 - add");
     JLabel m3 = new JLabel("2 - find");
@@ -17,13 +20,26 @@ public class View extends JFrame implements ActionListener {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600,600);
-        panel.setLayout(new GridLayout(5,1));
 
-        add(new Label("Menu"));
-        add(new Label("1 - add"));
-        add(new Label("1 - find"));
-        add(new Label("1 - update"));
-        add(new Label("1 - delete"));
+        main.setLayout(new BorderLayout());
+
+        header.setBorder(BorderFactory.createLineBorder(Color.RED));
+
+        header.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        body.setLayout(new GridLayout(4,1));
+        body.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+
+        header.add(m1);
+
+        body.add(m2);
+        body.add(m3);
+        body.add(m4);
+        body.add(m5);
+        main.add(header,BorderLayout.CENTER);
+        main.add(body,BorderLayout.WEST);
+        frame.add(main);
+
 
     }
 
